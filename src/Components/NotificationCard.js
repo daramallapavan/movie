707d93bcf8780115f5fd28a8d5f53c301d30a556
daramallapavan <daramallapavan123@example.com>
2024-08-18 11:34:19 +0530

    login chang 2

--- src/Components/NotificationCard.js
+++ src/Components/NotificationCard.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const NotificationCard = ({close,response}) => {
-
-    
   return (
    
-    <section className='fixed bg-neutral-700 lg:top-0 lg:right-2 w-80 top-0 right-0 bottom-o left-0  bg-opacity-50  z-40 flex justify-center items-center mx-2 mt-16 '>
+    <section className='fixed bg-neutral-700 top-0 right-2 w-80  bg-opacity-50  z-40 flex justify-center items-center mx-2 mt-16 '>
         
-<div className='bg-white w-full max-h-[10vh] max-w-screen-lg aspect-video rounded  relative  '>
-<button onClick={close} className=' absolute right-0 top-0   text-black '>Close</button>
+<div className='bg-green-400 w-full max-h-[10vh] max-w-screen-lg aspect-video rounded  relative  '>
+<button onClick={close} className='absolute right-0 top-0 lg:text-lg  text-red-900 '>Close</button>
 <h1 className='flex items-center justify-center text-black font-bold my-4 text-lg'>{response}</h1>
 </div>
 <div>
